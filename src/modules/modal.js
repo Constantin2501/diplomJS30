fd1d3d09154112be18a09f4fc3315d9dbbfd1f29
Konstantin Krotov <ckwebmast@gmail.com>
2023-06-25 14:47:17 +0300

    add button-services

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -6,12 +6,13 @@ const modal = () => {
 
 
     body.addEventListener('click', (e) => {
+        console.log(e.target);
         if (e.target.classList.contains('modal-close') || !e.target.closest('.modal-callback' )) {
             modalCallback.style.display = 'none'
             modalOverlay.style.display = 'none'
         }
 
-        if (e.target.classList.contains('modalBtn')) {
+        if (e.target.classList.contains('modalBtn')  || e.target.classList.contains('button-services')) {
             modalCallback.style.display = 'block'
             modalOverlay.style.display = 'block'
         }
