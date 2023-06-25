8b5f8d52196f51a6dcafd0c466211b13ab626343
Konstantin Krotov <ckwebmast@gmail.com>
2023-06-25 14:35:59 +0300

    fix close modal

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -2,10 +2,9 @@
 const modal = () => {
     const modalCallback = document.querySelector('.modal-callback')
     const modalOverlay = document.querySelector('.modal-overlay')
-    const modalBtn = document.querySelector('.modalBtn')
     const body = document.querySelector('body')
 
-    
+
     body.addEventListener('click', (e) => {
         console.log(e.target);
         if (e.target.classList.contains('modal-close') || !e.target.closest('.modal-callback' )) {
